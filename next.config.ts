4364f83886909df7055e17ec1a2beaf1dc06d40c
Sagar Sharma <kumaramansagar01@gmail.com>
2025-07-14 13:28:22 +0530

    code push

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/furnace",
+   assetPrefix: '/furnace/',
   distDir: "build",
   output: "standalone",
   typescript: {
